b1792e3811f77cf68d39053acf414622ece83039
Your Name <you@example.com>
2021-04-06 17:34:27 +0900

    설치하고 버튼누르면 서버에  message 이름값 하위에 특정한 규칙을 가진 child를 생성하면서 그 값을 "2"로 셋팅합니다.

--- app/src/main/java/gachon/mpclass/pearth/MainActivity.java
+++ app/src/main/java/gachon/mpclass/pearth/MainActivity.java
@@ -3,12 +3,30 @@ package gachon.mpclass.pearth;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button sendbt;
+
+    private FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+    private DatabaseReference databaseReference = firebaseDatabase.getReference();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sendbt = (Button) findViewById(R.id.button);
+
+        sendbt.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                databaseReference.child("message").push().setValue("2");
+            }
+        });
     }
 }
