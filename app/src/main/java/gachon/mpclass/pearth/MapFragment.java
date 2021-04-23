c6dc18f2c3398532a1ac587ad892c4d984e805f4
ssoyeong <jsy171771@gmail.com>
2021-04-24 03:01:48 +0900

    1. GrowingPlantActivity에 MapActivity와 SetLocationActivity를 연결해서 확인 2. strings.xml의 행정구역명 수정 --After reset HEAD-- 3. Modify MapActivity.java

---
+++ app/src/main/java/gachon/mpclass/pearth/MapFragment.java
@@ -0,0 +1,14 @@
+package gachon.mpclass.pearth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MapFragment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_map);
+    }
+}
