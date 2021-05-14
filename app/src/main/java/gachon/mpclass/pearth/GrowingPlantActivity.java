c887dde342760222b15fc78910927eef18b9a853
ssoyeong <jsy171771@gmail.com>
2021-05-15 01:03:20 +0900

    GrowingPlant 코드 정리

--- app/src/main/java/gachon/mpclass/pearth/GrowingPlantActivity.java
+++ app/src/main/java/gachon/mpclass/pearth/GrowingPlantActivity.java
@@ -33,7 +33,7 @@ import com.google.firebase.database.ValueEventListener;
 import java.util.Set;
 
 // 식물키우기 - 현재는 로그인 성공 후 화면 - 현재 해당 activity 기능: 로그아웃, 탈퇴
-public class GrowingPlantActivity extends AppCompatActivity implements View.OnClickListener {
+public class GrowingPlantActivity extends AppCompatActivity {
 
     private static final String TAG = "ProfileActivity";
 
@@ -43,9 +43,6 @@ public class GrowingPlantActivity extends AppCompatActivity implements View.OnCl
     DatabaseReference userDatabaseReference = mRootDatabaseReference.child("Users");
     //view objects
     private TextView textViewUserEmail;
-    private Button buttonLogout;
-    private TextView textivewDelete;
-    private Button buttonRest;
     private TextView textViewLevel;
     private TextView textViewScore;
     private ImageView imageViewPlant;
@@ -53,7 +50,6 @@ public class GrowingPlantActivity extends AppCompatActivity implements View.OnCl
     private Long report;
     private Long share;
     private int score = 0;
-    private User theUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,9 +58,6 @@ public class GrowingPlantActivity extends AppCompatActivity implements View.OnCl
 
         //initializing views
         textViewUserEmail = (TextView) findViewById(R.id.textviewUserEmail);
-        buttonLogout = (Button) findViewById(R.id.buttonLogout);
-        textivewDelete = (TextView) findViewById(R.id.textviewDelete);
-        buttonRest = (Button) findViewById(R.id.btnRest);
         textViewLevel = (TextView)findViewById(R.id.level);
         textViewScore = (TextView)findViewById(R.id.score);
         imageViewPlant = (ImageView)findViewById(R.id.plantImage);
@@ -84,178 +77,55 @@ public class GrowingPlantActivity extends AppCompatActivity implements View.OnCl
         textViewUserEmail.setText("반갑습니다.\n"+ user.getEmail()+"으로 로그인 하였습니다.");
 
 
-        buttonRest.setOnClickListener(this);
 
 
-        //유저 report, share 정보 가져오기
-        theUser = new User();
+
         String uid = user.getUid();
-
-
-
-
-                // report, share 수 가져오기
+        // report, share 수 가져오기
         userDatabaseReference.child(uid).addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
+
                 report = (Long)snapshot.child("report").getValue();
-                Log.d("score", "report 수: " + report);
-
-
-
                 share = (Long)snapshot.child("share").getValue();
-                Log.d("score", "share 수: " + share);
-//                score = score + Long.valueOf(share).intValue() * 20;
                 score = Long.valueOf(report).intValue() * 10 + Long.valueOf(share).intValue() * 20;
-                Log.d("score", "score: " + score);
+
                 textViewScore.setText(String.valueOf(score));
 
-
-//                theUser.setReport(Long.valueOf(report).intValue());
-//                Toast.makeText(GrowingPlantActivity.this, "theUser report 수: "+ theUser.getReport(), Toast.LENGTH_LONG).show();
-
-
-//                report_num = Long.valueOf(report).intValue();
-//                score = report * 10;
-//                Toast.makeText(GrowingPlantActivity.this, "report 점수: "+ score, Toast.LENGTH_LONG).show();
+                if(0<= score && score < 20){
+                    textViewLevel.setText("Lv. 1");
+                    imageViewPlant.setImageResource(R.drawable.plant_1);
+                }
+                else if(20<= score && score < 40){
+                    textViewLevel.setText("Lv. 2");
+                    imageViewPlant.setImageResource(R.drawable.plant_2);
+                }
+                else if(40<= score && score < 60){
+                    textViewLevel.setText("Lv. 3");
+                    imageViewPlant.setImageResource(R.drawable.plant_3);
+                }
+                else if(60<= score && score < 80){
+                    textViewLevel.setText("Lv. 4");
+                    imageViewPlant.setImageResource(R.drawable.plant_3);        //todo: 식물사진 추가
+                }
+                else if(80<= score && score < 100){
+                    textViewLevel.setText("Lv. 5");
+                    imageViewPlant.setImageResource(R.drawable.plant_3);
+                }
+                else if(100<= score){
+                    textViewLevel.setText("Lv. 6");
+                    imageViewPlant.setImageResource(R.drawable.plant_3);
+                }
             }
-
 
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
                 throw error.toException();
             }
         });
-
-
-
-
-
-
-//
-//
-//        // report 가져오기 및 설정     todo: share도 반영
-//        userDatabaseReference.child(uid).child("report").addListenerForSingleValueEvent(new ValueEventListener() {
-//            @Override
-//            public void onDataChange(@NonNull DataSnapshot snapshot) {
-//                report = (Long)snapshot.getValue();
-//                Log.d("score", "report 수: " + report);
-//                score = Long.valueOf(report).intValue() * 10;
-//                Log.d("score", "score: " + score);
-////                textViewScore.setText(Long.valueOf(score).intValue());
-//                textViewScore.setText(String.valueOf(score));
-//
-//                if(0<= score && score < 20){
-//                    textViewLevel.setText("Lv. 1");
-//                    imageViewPlant.setImageResource(R.drawable.plant_1);
-//                }
-//                else if(20<= score && score < 40){
-//                    textViewLevel.setText("Lv. 2");
-//                    imageViewPlant.setImageResource(R.drawable.plant_2);
-//                }
-//                else if(40<= score && score < 60){
-//                    textViewLevel.setText("Lv. 3");
-//                    imageViewPlant.setImageResource(R.drawable.plant_3);
-//                }
-//                else if(60<= score && score < 80){
-//                    textViewLevel.setText("Lv. 4");
-//                    imageViewPlant.setImageResource(R.drawable.plant_3);        //todo: 식물사진 추가
-//                }
-//                else if(80<= score && score < 100){
-//                    textViewLevel.setText("Lv. 5");
-//                    imageViewPlant.setImageResource(R.drawable.plant_3);
-//                }
-//                else if(100<= score){
-//                    textViewLevel.setText("Lv. 6");
-//                    imageViewPlant.setImageResource(R.drawable.plant_3);
-//                }
-//            }
-//
-//
-//            @Override
-//            public void onCancelled(@NonNull DatabaseError error) {
-//                throw error.toException();
-//            }
-//        });
-//
-//
-//
-//
-//
-//        // share 수 가져오기
-//        userDatabaseReference.child(uid).child("share").addListenerForSingleValueEvent(new ValueEventListener() {
-//            @Override
-//            public void onDataChange(@NonNull DataSnapshot snapshot) {
-//                share = (Long)snapshot.getValue();
-//            }
-//
-//            @Override
-//            public void onCancelled(@NonNull DatabaseError error) {
-//                throw error.toException();
-//            }
-//        });
-//        Log.d("score", "share 수: " + share);
-
-
-
-
-
-
-
-
-        // 식당 추천 임시 버튼
-        buttonRest = (Button) findViewById(R.id.btnRest);
-
-        Animation anim = AnimationUtils.loadAnimation(GrowingPlantActivity.this, R.anim.transparent);
-        buttonRest.startAnimation(anim);
-
-        buttonRest.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(GrowingPlantActivity.this, SetLocationActivity.class);
-                startActivity(intent);
-                finish();
-
-            }
-        });
-
     }
 
 
-    public void onClick(View view) {
-        if (view == buttonLogout) {
-            firebaseAuth.signOut();
-            finish();
-            startActivity(new Intent(this, LoginActivity.class));
-        }
-        //회원탈퇴를 클릭하면 회원정보를 삭제한다. 삭제전에 컨펌창을 하나 띄워야 겠다.
-        if(view == textivewDelete) {
-            AlertDialog.Builder alert_confirm = new AlertDialog.Builder(GrowingPlantActivity.this);
-            alert_confirm.setMessage("정말 계정을 삭제 할까요?").setCancelable(false).setPositiveButton("확인", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialogInterface, int i) {
-                            FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-                            user.delete()
-                                    .addOnCompleteListener(new OnCompleteListener<Void>() {
-                                        @Override
-                                        public void onComplete(@NonNull Task<Void> task) {
-                                            Toast.makeText(GrowingPlantActivity.this, "계정이 삭제 되었습니다.", Toast.LENGTH_LONG).show();
-                                            finish();
-                                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
-                                        }
-                                    });
-                        }
-                    }
-            );
-            alert_confirm.setNegativeButton("취소", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialogInterface, int i) {
-                    Toast.makeText(GrowingPlantActivity.this, "취소", Toast.LENGTH_LONG).show();
-                }
-            });
-            alert_confirm.show();
-        }
-    }
     //액션버튼 메뉴 액션바에 집어 넣기
     @Override
     public boolean onCreateOptionsMenu (Menu menu){
