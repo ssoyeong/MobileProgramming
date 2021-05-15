2491ec71d83d3f59b623d73a9b86912cdd15159e
yekini9908 <82139188+yekini9908@users.noreply.github.com>
2021-05-16 00:58:35 +0900

    검색 버튼 + 새로고침

--- app/src/main/java/gachon/mpclass/pearth/searchActivity.java
+++ app/src/main/java/gachon/mpclass/pearth/searchActivity.java
@@ -23,42 +23,30 @@ public class searchActivity extends AppCompatActivity {
     postfragment Postfragment;
     nosearchfragment Nosearchfragment;
     String key;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post_search);
         getSupportActionBar().setTitle("검색");
-        Button search = (Button) findViewById(R.id.search);
-        EditText keyword = (EditText) findViewById(R.id.keyword);
 
-        Nosearchfragment = new nosearchfragment();
-        G.keyword = keyword.getText().toString();
-        key = G.keyword;
-        getSupportFragmentManager().beginTransaction().replace(R.id.container, Nosearchfragment).commit();
         Postfragment = new postfragment();
 
-        search.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
 
-                    G.keyword = keyword.getText().toString();
-                    getSupportFragmentManager().beginTransaction().replace(R.id.container, Postfragment).commit();
-                    keyword.setText("");
-
-            }
-        });
-
-
-    }
-
-    public void onFragmentChanged(int index) {
-        if (index == 0) {
-            getSupportFragmentManager().beginTransaction().replace(R.id.container, Nosearchfragment).commit();
-        }
-
-        if (index == 1) {
         getSupportFragmentManager().beginTransaction().replace(R.id.container, Postfragment).commit();
+
+
     }
 
-
-}}
+//    public void onFragmentChanged(int index) {
+//        if (index == 0) {
+//            getSupportFragmentManager().beginTransaction().replace(R.id.container, Nosearchfragment).commit();
+//        }
+//
+//        if (index == 1) {
+//            getSupportFragmentManager().beginTransaction().replace(R.id.container, Postfragment).commit();
+//        }
+//
+//
+//    }
+}
